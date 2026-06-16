636c0a502e4ea7f3483a37653fbc07818555d659
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-16 21:31:56 +0200

    Remove css/assets from vite and create components directory

--- app/frontend/src/pages/LoginPage.tsx
+++ app/frontend/src/pages/LoginPage.tsx
@@ -1,41 +1,11 @@
 import { useState } from 'react'
+import LoginForm from '../components/LoginForm'
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    console.log({ email, password })
-  }
-
   return (
-    <div className="page">
-        <h1>3D Tic-Tac-Toe</h1>
-
-        <form className="loginForm" onSubmit={handleSubmit}>
-          <div className="field">
-            <label>Email</label>
-            <input
-              type="email"
-              placeholder="you@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div className="field">
-            <label>Password</label>
-            <input
-              type="password"
-              placeholder="••••••••"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <button className="submitButton" type="submit">Sign in</button>
-        </form>
+    <div className="loginPage">
+      <h1>3D Tic-Tac-Toe</h1>
+      <LoginForm />
     </div>
   )
 }

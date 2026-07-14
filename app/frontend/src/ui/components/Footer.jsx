098e2bd537569dc61351d452cb6411f8e74e847c
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-14 15:57:03 +0200

    Remove username from URL and read it from context

--- app/frontend/src/ui/components/Footer.jsx
+++ app/frontend/src/ui/components/Footer.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
 
-const Footer = ({username}) => {
+const Footer = () => {
   return (
     <footer className="flex justify-between items-center p-8 border-t border-stone-400 text-xs">
       <p>© 3D-tic-tac-toe {new Date().getFullYear()}</p>
       <div className="flex gap-8">
-        <Link to={`/privacy?user=${username}`}>Privacy Policy</Link>
+        <Link to={"/privacy"}>Privacy Policy</Link>
         <Link to="/terms">Terms of Service</Link>
       </div>
     </footer>

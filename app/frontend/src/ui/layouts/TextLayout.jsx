4d5be072077ab7594972066c61eda665bee2b29b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-14 20:56:19 +0200

    Write what data we collect in privacy policy

--- app/frontend/src/ui/layouts/TextLayout.jsx
+++ app/frontend/src/ui/layouts/TextLayout.jsx
@@ -5,7 +5,7 @@ const TextLayout = ({children}) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 flex flex-col items-center px-10 sm:px-20 md:px-40 lg:px-80 py-10">{children}</main>
+      <main className="flex-1 flex flex-col items-center px-10 sm:px-20 md:px-40 lg:px-80 py-10 gap-4">{children}</main>
       <Footer />
     </div>
   )
--- app/frontend/src/ui/pages/PrivacyPolicy.jsx
+++ app/frontend/src/ui/pages/PrivacyPolicy.jsx
@@ -3,8 +3,15 @@ import TextLayout from '../layouts/TextLayout'
 const PrivacyPolicy = () => {
   return (
     <TextLayout>
-      <h1>Privacy Policy</h1>
+      <h1 className="text-4xl">Privacy Policy</h1>
       <p>This Privacy Policy describes how your personal information is collected and used when you use this web app.</p>
+      <h2 className="text-2xl">What we collect</h2>
+      <ul>
+        <li> - Account credentials: email, username, hashed password (your password is not stored in plain text).</li>
+        <li> - Game data: moves, board state, opponenets, results for every game you play.</li>
+      </ul>
+      <h2 className="text-2xl">Contact</h2>
+      <p>simone.gasparini.2@gmail.com</p>
     </TextLayout>
   )
 }

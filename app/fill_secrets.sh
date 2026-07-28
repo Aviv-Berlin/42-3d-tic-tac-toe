ced964923e143f57f8cda1c7f0105bd99eb3d333
Hilary Allison <165587952+hilary-allison@users.noreply.github.com>
2026-07-28 15:59:29 +0200

    feat: change fill_secrets.sh to use env already set in /app/backend/.env

--- app/fill_secrets.sh
+++ app/fill_secrets.sh
@@ -4,24 +4,56 @@
 
 set -euo pipefail
 
-env_file="./backend/postgres/postgres.env"
+# For the smoothest transition to using Docker for postgres,
+# we will fill Postgres's required ENV from the backend .env
 
+# First checking that the file exists
+if [ ! -s "./backend/.env" ]
+then
+	printf "Missing ./backend/.env. Please create and fill this file:
+		SECRET=
+		PORT=
+		DB_HOST=
+		DB_PORT=
+		DB_NAME=
+		DB_USER=
+		DB_PASSWORD=
+	"
+	exit 1
+fi
+
+# And checking that it contains the ENV we need
+source ./backend/.env
+if [ -z "$DB_USER" ] || [ -z "$DB_NAME" ]
+then
+	printf "Please fill DB_USER and DB_NAME in ./backend/.env"
+	exit 1
+fi
+
+# ENV for Postgres
+postgres_env_file="./backend/postgres/postgres.env"
 printf "POSTGRES ENV:\n"
 if [ ! -s ./backend/postgres/postgres.env ]
 # s = size. Here checking if file is nonexistant or empty
 then
-	touch ${env_file}
+	touch ${postgres_env_file}
 	printf "postgres.env file created\n"
-	read -p "Enter POSTGRES_USER (new postgres user, owns the database): " postgres_user
-	echo "POSTGRES_USER=${postgres_user}" >> ${env_file}
-	read -p "Enter POSTGRES_DB (database name): " postgres_db
-	echo "POSTGRES_DB=${postgres_db}" >> ${env_file}
 
+	# May return to this approach at a later date
+	#read -p "Enter POSTGRES_USER (new postgres user, owns the database): " postgres_user
+	#echo "POSTGRES_USER=${postgres_user}" >> ${postgres_env_file}
+	#read -p "Enter POSTGRES_DB (database name): " postgres_db
+	#echo "POSTGRES_DB=${postgres_db}" >> ${postgres_env_file}
+	
+	echo "POSTGRES_DB=${DB_NAME}" >> ${postgres_env_file}
+	echo "POSTGRES_USER=${DB_USER}" >> ${postgres_env_file}
 else
-	printf "Found existing file postgres.env.\n\n"
+	printf "Found existing postgres.env file.\n\n"
 	printf "The contents will be used by Docker Compose for your database ENV.\n\n"
 fi
 
+
+# SECRETS for Postgres
 printf "SECRETS:\n"
 if [ ! -s ./secrets/postgres-passwd ]
 then

1533daae4b8511625a79dbf076fc7ebe9769576c
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-29 19:53:10 +0200

    Create new context for gameData

---
+++ app/frontend/src/ui/context/GameDataContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from 'react'
+import { PropsWithChildren } from 'react'
+import { GameData } from '../../types/game'
+
+interface GameDataContextType {
+  gameData: GameData | null;
+  setGameData: React.Dispatch<React.SetStateAction<GameData | null>>;
+}
+
+const GameDataContext = createContext<GameDataContextType | null>(null)
+
+const GameDataProvider = ({ children }: PropsWithChildren) => {
+  const [gameData, setGameData] = useState<GameData | null>(null)
+
+  return (
+    <GameDataContext.Provider value={{ gameData, setGameData }}>
+      {children}
+    </GameDataContext.Provider>
+  )
+}
+
+const useGameData = () => useContext(GameDataContext);
+
+export { GameDataProvider, useGameData }

edffff4e8ed7b79890ad7defb98967aad1235244
Aviv Kosloff <aviv.film@gmail.com>
2026-07-29 15:30:35 +0200

    created a reply function

--- app/frontend/src/game/GameState.ts
+++ app/frontend/src/game/GameState.ts
@@ -3,6 +3,7 @@ import { checkWin } from "./GameCheckWin";
 import { GameGraphics } from "./GameGraphics";
 import { GridPosition, CellState, PLAYER_STATES} from "./Types";
 import { Player } from "./Player";
+import { LocalPlayer } from "./LocalPlayer";
 import { GameData, Move } from "../types/game";
 
 
@@ -58,7 +59,25 @@ export class GameState {
         this.getCurrentPlayer().yourTurn(this.boardState, this.N, this.getCurrentPlayerState());
     }
 
-
+    public async startReply(): Promise<void> {
+        for (let i = 0; i < this.gameData.moves.length; i++) {
+            if (i % 2 === 0) {
+                await this.ui.playerTitle(this.gameData.player1.username);
+            } else {
+                await this.ui.playerTitle(this.gameData.player2.username);
+            }
+            this.boardState[this.gameData.moves[i].pos.x][this.gameData.moves[i].pos.y][this.gameData.moves[i].pos.z] = this.gameData.moves[i].player;
+            setTimeout(() => { this.graphics.placeSphere(this.gameData.moves[i].pos, this.gameData.moves[i].player);}, 500);
+        }
+        const winningPositions = checkWin(this.boardState, this.gameData.moves[this.gameData.moves.length -1].pos, this.gameData.moves[this.gameData.moves.length -1].player, this.N);
+        if (winningPositions)
+            this.graphics.animateWin(winningPositions);
+        if (this.gameData.moves[this.gameData.moves.length -1].player === CellState.Player1)
+            this.ui.displayWinner(this.gameData.player1.username);
+        else
+            this.ui.displayWinner(this.gameData.player2.username);
+         this.exitTimeout = setTimeout(() => { this.onExit();}, 3000);
+    }
 
     public placeMove(pos: GridPosition): boolean {
         if (this.gameOver)

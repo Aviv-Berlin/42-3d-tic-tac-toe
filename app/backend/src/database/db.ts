884108721a488c3e199c1f9df43cfd2f9543b4e2
Hilary Allison <165587952+hilary-allison@users.noreply.github.com>
2026-07-31 15:33:18 +0200

    feat: in db.ts, replace reading of password from .env to /secrets/postgres-passwd file. Remove DB_PASSWORD from instructions pertaining to .env

--- app/backend/src/database/db.ts
+++ app/backend/src/database/db.ts
@@ -1,22 +1,16 @@
 import { Pool } from "pg";
-import { readFile } from 'node:fs/promises';
+import { readFile } from 'node:fs/promises'; // API for async file operations in Node.js
 import path from 'node:path';
 
 const filePath = path.join(import.meta.dirname, '../../../secrets/postgres-passwd')
 
-// source https://www.xjavascript.com/blog/typescript-read-file/
-// what is an async function https://www.theodinproject.com/lessons/node-path-javascript-async-and-await
-// why would we want this to be asyncronous, as recommended by first source
-
-async function readPostGresPassword(): Promise<void> {
-
-}
+const pw = await readFile(filePath, 'utf8');
 
 console.log({
 	DB_USER: process.env.DB_USER,
 	DB_HOST: process.env.DB_HOST,
 	DB_NAME: process.env.DB_NAME,
-	DB_PASSWORD: process.env.DB_PASSWORD,
+	DB_PASSWORD: "****",
 	DB_PORT: process.env.DB_PORT,
 })
 
@@ -24,7 +18,7 @@ const pool = new Pool({
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
+	password: pw,
 	port: Number(process.env.DB_PORT),
 });
 

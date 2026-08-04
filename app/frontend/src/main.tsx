a4b394a4d2a556e3dd14fd11f4e5acfef47562b1
Aviv Kosloff <aviv.film@gmail.com>
2026-08-04 15:10:29 +0200

    Replay class from main

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,19 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { UsernameProvider } from './ui/context/UsernameContext'
-import { GameDataProvider } from './ui/context/GameDataContext'
 import App from './ui/App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UsernameProvider>
-      <GameDataProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </GameDataProvider>
-    </UsernameProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )

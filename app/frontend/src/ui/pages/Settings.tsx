5d22e5ef959a5299a9fa9213e4c3b7b205cca4a2
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-04 14:56:31 +0200

    Add header and buttons for modifying user data

--- app/frontend/src/ui/pages/Settings.tsx
+++ app/frontend/src/ui/pages/Settings.tsx
@@ -1,9 +1,27 @@
+import SecondaryButton from '../components/SecondaryButton';
 import MainLayout from '../layouts/MainLayout';
 
 const Settings = () => {
+  const handleChangeUsername = () => {
+    console.log("change username");
+  }
+
+  const handleChangePassword = () => {
+    console.log("change password");
+  }
+
+  const handleDeleteAccount = () => {
+    console.log("delete account");
+  }
+
   return (
     <MainLayout>
-      <h1>Settings</h1>
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="text-3xl font-serif italic">Settings</h1>
+        <SecondaryButton onClick={handleChangeUsername}>Change username</SecondaryButton>
+        <SecondaryButton onClick={handleChangePassword}>Change password</SecondaryButton>
+        <SecondaryButton onClick={handleDeleteAccount}>Delete account</SecondaryButton>
+      </div>
     </MainLayout>
   )
 }

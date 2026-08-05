6dc7bcb4eb7707097c09a0195bb7e821604f55dd
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-05 14:42:23 +0200

    Create layout for profile page

---
+++ app/frontend/src/ui/layouts/ProfileLayout.tsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { PropsWithChildren } from 'react'
+
+const ProfileLayout = ({children}: PropsWithChildren) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 bg-grid grid grid-cols-1 sm:grid-cols-[2fr_1fr]">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default ProfileLayout
--- app/frontend/src/ui/pages/Profile.tsx
+++ app/frontend/src/ui/pages/Profile.tsx
@@ -1,10 +1,50 @@
-import MainLayout from '../layouts/MainLayout';
+import ProfileLayout from '../layouts/ProfileLayout';
+import { GameData } from '../../types/game'
 
 const Profile = () => {
+
+  // this array will need to be fetched from the backend
+  // im using the same interface that gets passed to babylon
+  const games: GameData[] = [
+    {
+      player1: {
+        type: "real",
+        username: "s-gas",
+      },
+      player2: {
+        type: "real",
+        username: "a_random_player",
+      },
+      level: 0,
+      winner: {
+        type: "real",
+        username: "s-gas",
+      },
+      moves: [],
+      size: 3,
+      isFinished: true,
+      isDraw: false,
+      gameStart: 0,
+      gameEnd: 0,
+    }
+  ];
+
   return (
-    <MainLayout>
-      <h1>Profile</h1>
-    </MainLayout>
+    <ProfileLayout>
+      <div className="border-r border-stone-400 p-8 flex flex-col gap-4">
+        <h2 className="text-2xl font-serif italic">Recent Games</h2>
+        {games.map((game, i) => (
+          <div key={i} className="border rounded-md border-stone-400 p-4 flex gap-4">
+            <p>WIN</p>
+            <p>against AI</p>
+            <p>Board size: 3x3x3</p>
+          </div>
+        ))}
+      </div>
+      <div className="p-8">
+        <h2 className="text-2xl font-serif italic">Stats</h2>
+      </div>
+    </ProfileLayout>
   )
 }
 

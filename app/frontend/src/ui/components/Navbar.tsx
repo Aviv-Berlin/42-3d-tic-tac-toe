7fd3b74512982c83f8b88545dc15c31efa1315a4
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-14 14:26:48 +0200

    Add 3d cube in auth pages

--- app/frontend/src/ui/components/Navbar.tsx
+++ app/frontend/src/ui/components/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   return (
     <nav className="flex flex-col sm:flex-row items-center sm:justify-between p-4 border-b border-stone-400">
       <Link to={"/home"} className="px-4 py-2 rounded-md hover:bg-stone-200 cursor-pointer flex gap-2 items-center">
-        <div className="w-10 h-10 rounded-md flex justify-center items-center relative overflow-hidden">
-          <BabylonImage type={"three"} />
+        <div className="w-8 h-8 rounded-md relative overflow-hidden">
+          <BabylonImage type="three" />
         </div>
         <p className="hidden sm:inline font-bold">3D tic-tac-toe</p>
       </Link>

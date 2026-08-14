b3e59ef3f7456e2e79c80fcebfbf3b6564dd1fab
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-14 11:37:06 +0200

    Start and stop also two instances of the frontend

--- run.sh
+++ run.sh
@@ -5,3 +5,8 @@ root=$(pwd);
 cd app/backend;
 
 npm run dev & echo $! > ${root}/.backend.pid;
+
+cd ../frontend;
+
+npm run dev & echo $! > ${root}/.frontend1.pid;
+npm run dev:alt & echo $! > ${root}/.frontend2.pid;
--- stop.sh
+++ stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 kill $(cat .backend.pid);
+kill $(cat .frontend1.pid);
+kill $(cat .frontend2.pid);
 
-rm .backend.pid;
+rm .{backend,frontend{1,2}}.pid;

2e3c1de4b24c4ae4dbdd3e609216749f24eb322e
Steven Moon <steven_moon@live.com>
2026-08-21 17:41:00 +0200

    add win/loss/draw queries

--- app/backend/src/database/statsQueries.ts
+++ app/backend/src/database/statsQueries.ts
@@ -6,17 +6,29 @@ export async function getUserGames(id: number) {
 	const result = await query(
 		'SELECT * FROM matches WHERE player1 = $1 OR player2 = $1 ORDER BY started_at DESC;', [id]
 	);
-
 	return result;
 }
 
-// export async function getUserWins(id: number) {
-// 	const result = await query(
-// 		'SELECT * FROM matches WHERE player1 = $1 OR player2 = $1 ORDER BY started_at DESC;', [id]
-// 	);
+export async function getUserWins(id: number) {
+	const result = await query(
+		'SELECT COUNT(*) FROM matches WHERE (player1 = $1 OR player2 = $1) AND winner = $1;', [id]
+	);
+	return result.rows[0];
+}
 
-// 	return result;
-// }
+export async function getUserDraws(id: number) {
+	const result = await query(
+		'SELECT COUNT(*) FROM matches WHERE (player1 = $1 OR player2 = $1) AND winner == null;', [id]
+	);
+	return result.rows[0];
+}
+
+export async function getUserLosses(id: number) {
+	const result = await query(
+		'SELECT COUNT(*) FROM matches WHERE (player1 = $1 OR player2 = $1) AND winner != null AND winner != $1;', [id]
+	);
+	return result.rows[0];
+}
 
 export async function getMatchReplay(id: number) {
 	const result = await query(

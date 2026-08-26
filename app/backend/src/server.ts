14607ff76a96fd523842b07064b7b7f8d21b8c3b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-26 13:07:50 +0200

    Create service for sending request

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { response } from "express";
+import express from "express";
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 import http from "http";
--- app/frontend/src/ui/routes/ProtectedRoute.tsx
+++ app/frontend/src/ui/routes/ProtectedRoute.tsx
@@ -1,11 +1,22 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import axios from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import meService from "../../services/me"
 
 const ProtectedRoute = () => {
-	const loggedIn = window.localStorage.getItem('username')
-	if (!loggedIn) {
-  	  return <Navigate to="/login" replace />
-  	}
+  const navigate = useNavigate();
+
+  const checkToken = async () => {
+    try {
+      const response = await meService.getUsername();
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+      navigate("/login");
+    }
+  }
+  useEffect(() => {
+    checkToken();
+  })
 	return <Outlet />
 }
 

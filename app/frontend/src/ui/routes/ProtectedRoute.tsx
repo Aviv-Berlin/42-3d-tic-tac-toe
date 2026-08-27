78317315ec4c3817b4f3d22ccd896138a7492c5b
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-27 11:28:57 +0200

    Add loading screen

--- app/frontend/src/ui/routes/ProtectedRoute.tsx
+++ app/frontend/src/ui/routes/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import meService from "../../services/me"
 import { useSetUsername } from '../../store/username';
@@ -6,12 +6,15 @@ import { useSetUsername } from '../../store/username';
 const ProtectedRoute = () => {
   const navigate = useNavigate();
   const setUsername = useSetUsername();
+  const [retrieved, setRetrieved] = useState(false);
 
   const checkToken = async () => {
     try {
       const response = await meService.getUsername();
       setUsername(response.data.username);
+      setRetrieved(true);
     } catch (err) {
+      setRetrieved(false);
       console.log(err);
       navigate("/login");
     }
@@ -19,7 +22,9 @@ const ProtectedRoute = () => {
   useEffect(() => {
     checkToken();
   }, [])
-	return <Outlet />
+
+  if (retrieved) return <Outlet />
+  return <p>LOADING</p>
 }
 
 export default ProtectedRoute

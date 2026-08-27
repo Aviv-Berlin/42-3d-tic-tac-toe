de43c7c6f0bedb64546da12dd7666a9b3021db75
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-27 09:17:32 +0200

    Update username after every protected route

--- app/frontend/src/ui/routes/ProtectedRoute.tsx
+++ app/frontend/src/ui/routes/ProtectedRoute.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import meService from "../../services/me"
+import { useSetUsername } from '../../store/username';
 
 const ProtectedRoute = () => {
   const navigate = useNavigate();
 
   const checkToken = async () => {
+    const setUsername = useSetUsername();
     try {
-      await meService.getUsername();
+      const response = await meService.getUsername();
+      setUsername(response.data.username);
     } catch (err) {
       console.log(err);
       navigate("/login");

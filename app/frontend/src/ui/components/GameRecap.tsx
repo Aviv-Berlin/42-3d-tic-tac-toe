5141dbd84a42eb9c24c5a6bb1e629b8433a0b26d
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-09-01 15:17:20 +0200

    Resize text in Profile and add number of games displayed

--- app/frontend/src/ui/components/GameRecap.tsx
+++ app/frontend/src/ui/components/GameRecap.tsx
@@ -8,10 +8,10 @@ const GameRecap = ( { gameHistory }: GameRecapProps) => {
   const size = gameHistory.size;
   return (
     <div className="border bg-light-grey px-8 py-4 flex flex-row justify-between items-center">
-      <p className="text-xl">{gameHistory.outcome}</p>
+      <p className="text-l">{gameHistory.outcome}</p>
       <div className="hidden sm:flex flex-row gap-12">
-        <p className="text-sm">{`Opponent: ${ gameHistory.opponent }`}</p>
-        <p className="text-sm">{`Board size: ${size}x${size}x${size}`}</p>
+        <p className="text-xs">{`Opponent: ${ gameHistory.opponent }`}</p>
+        <p className="text-xs">{`Board size: ${size}x${size}x${size}`}</p>
       </div>
     </div>
   )

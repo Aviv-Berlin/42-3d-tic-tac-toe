f09ec01f8aaf35603e7d37387b6f119e2b5162f8
Steven Moon <steven_moon@live.com>
2026-09-09 12:09:15 +0200

    Display difficulty next to ai name on profile history

--- app/frontend/src/ui/components/GameRecap.tsx
+++ app/frontend/src/ui/components/GameRecap.tsx
@@ -11,14 +11,32 @@ const GameRecap = ( { gameHistory }: GameRecapProps) => {
   const size = gameHistory.size;
   const navigate = useNavigate();
   const setGameData = useSetGameData();
+  let opponent = gameHistory.opponent;
+  if (opponent === "ai") {
+    let level = "";
+    switch (gameHistory.gameData.level) {
+      case 1:
+        level = "easy";
+        break;
+      case 2:
+        level = "medium";
+        break;
+      case 3:
+        level = "hard";
+        break;
+      default:
+        console.log("ai difficulty couldn't be found");
+    }
+    if (level)
+      opponent = `ai (${level})`
+  }
 
   return (
     <div className="border bg-light-grey px-8 py-4 flex flex-row justify-between items-center">
       <p className="text-l">{gameHistory.outcome}</p>
       <div className="hidden sm:flex flex-row gap-12">
-        <p className="text-xs">{`Opponent: ${ gameHistory.opponent }`}</p>
+        <p className="text-xs">{`Opponent: ${opponent}`}</p>
         <p className="text-xs">{`Board size: ${size}x${size}x${size}`}</p>
-        <p className="text-xs">{`Difficulty: ${gameHistory.gameData.level}`}</p>
       </div>
 	  <SecondaryButton onClick={() => {
 		setGameData(gameHistory.gameData);

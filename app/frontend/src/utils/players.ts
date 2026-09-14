3d5cb1be308a54b4017a2a8a3777eb80600a2c30
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-09-14 15:43:59 +0200

    Remove commented interface

--- app/frontend/src/utils/players.ts
+++ app/frontend/src/utils/players.ts
@@ -1,30 +1,4 @@
-import { PlayerData, GameMode } from '../../../shared/game';
-
-// const createPlayers = (username: string, gameMode: GameMode): PlayerData[] => {
-//   const player1: PlayerData = {
-//     type: "real",
-//     username,
-//   }
-
-//   let player2: PlayerData;
-//   if (gameMode === "online") {
-//     player2 = {
-//       type: "real",
-//       username: "stranger",
-//     }
-//   } else if (gameMode === "ai") {
-//     player2 = {
-//       type: "ai",
-//       username: "ai",
-//     }
-//   } else {
-//     player2 = {
-//       type: "guest",
-//       username: "guest",
-//     }
-//   }
-//   return [player1, player2];
-// }
+import { PlayerData, GameMode } from '../../../shared/game.js';
 
 const createPlayers = (players: string[], gameMode: GameMode): PlayerData[] => {
   const player1: PlayerData = {
--- app/shared/messages.ts
+++ app/shared/messages.ts
@@ -1,8 +1,7 @@
-import { GridPosition, CellState, PLAYER_STATES} from "./game/Types";
+import { GridPosition, CellState } from "./game/Types";
 import { GameData } from "./game";
 import { Match } from "../backend/src/controllers/gameController"
-import { PlayerConnection } from "../backend/src/websocket/matchSockets";
-import { GameState } from "../backend/src/game/GameState"; 
+import { GameState } from "../backend/src/game/GameState";
 
 //client -> server
 export type LeaveMatchMessage =

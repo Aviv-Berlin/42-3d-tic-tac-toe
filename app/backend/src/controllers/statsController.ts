e93ee29c3c9116e51c39aeeb09162cb4babc738f
Steven Moon <steven_moon@live.com>
2026-09-16 13:32:50 +0200

    delete separate getters for win/loss/draw counts

--- app/backend/src/controllers/statsController.ts
+++ app/backend/src/controllers/statsController.ts
@@ -118,62 +118,6 @@ export async function getGameHistory(request: ExtdRequest, response: Response) {
 	}
 }
 
-export async function getWinTotal(request: ExtdRequest, response: Response) {
-	const id = request.userData?.id;
-	if (!id) {
-		return response.status(400).json({
-			error: 'no user id in token'
-		});
-	}
-	try {
-		const wins = await statsQueries.getUserWins(id);
-		return response.status(200).json(wins);
-	}
-	catch (error) {
-		console.error(error);
-		return response.status(500).json({
-			error: 'internal server error'
-		});
-	}
-}
-
-export async function getDrawTotal(request: ExtdRequest, response: Response) {
-	const id = request.userData?.id;
-	if (!id) {
-		return response.status(400).json({
-			error: 'no user id in token'
-		});
-	}
-	try{
-		const draws = await statsQueries.getUserDraws(id);
-		return response.status(200).json(draws);
-	}
-	catch (error) {
-		console.error(error);
-		return response.status(500).json({
-			error: 'internal server error'
-		});
-	}
-}
-
-export async function getLossTotal(request: ExtdRequest, response: Response) {
-	const id = request.userData?.id;
-	if (!id)
-		return response.status(400).json({
-			error: 'no user id in token'
-		});
-	try{
-		const losses = await statsQueries.getUserLosses(id);
-		return response.status(200).json(losses);
-	}
-	catch (error) {
-		console.error(error);
-		return response.status(500).json({
-			error: 'internal server error'
-		});
-	}
-}
-
 export async function getRankData(request: ExtdRequest, response: Response) {
 	const id = request.userData?.id;
 	if (!id)
@@ -212,9 +156,6 @@ export async function getUserStats(request: ExtdRequest, response: Response) {
 
 export default {
 	getGameHistory,
-	getWinTotal,
-	getDrawTotal,
-	getLossTotal,
 	getRankData,
 	getUserStats
 };
--- app/backend/src/database/statsQueries.ts
+++ app/backend/src/database/statsQueries.ts
@@ -61,9 +61,6 @@ export async function getUserGameStats(id: number) {
 
 export default {
 	getUserGames,
-	getUserWins,
-	getUserDraws,
-	getUserLosses,
 	getMatchReplay,
 	getRecentGames,
 	getUserGameStats

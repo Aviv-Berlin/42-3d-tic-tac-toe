e53a3010962505a1fac15a3491d6d97029af2a3f
h-allison <165587952+h-allison@users.noreply.github.com>
2026-09-17 12:08:02 +0200

    feat: implement 302 redirect in vite.config for already-logged-in user who tries to access login

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,12 +1,33 @@
 import { defineConfig } from 'vite'
+import { parse } from 'cookie'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/guide/api-plugin.html#configureserver
+const myPlugin = () => ({
+  name: 'token_check_before_login',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url === '/login') {
+        const cookies = parse(req.headers.cookie || '')
+        if (cookies['token']) {
+          res.statusCode = 302
+          res.setHeader('Location', '/home')
+          res.end()
+          return
+        }
+      }
+      next()
+    })
+  },
+})
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    myPlugin(),
   ],
   server: {
     proxy: {
@@ -23,3 +44,18 @@ export default defineConfig({
     }
   }
 })
+
+/*
+app.use((req, res, next) => {
+
+    if (req.path.startsWith('/v1') || req.path.startsWith('/login')) return next();
+    if (req.path.match(/\.\w+$/)) {
+        return next();
+    }
+    const cookie = req.cookies;
+    if (!cookie || !cookie['token']) {
+        return (res.redirect(302, '/login'));
+    }
+    next();
+});
+*/

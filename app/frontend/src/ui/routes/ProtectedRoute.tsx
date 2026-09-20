3880a6bb76124cd0304e54dc1c8609951bdb260f
h-allison <165587952+h-allison@users.noreply.github.com>
2026-09-20 16:11:35 +0200

    chore: remove commented-out copy of old protected route

--- app/frontend/src/ui/routes/ProtectedRoute.tsx
+++ app/frontend/src/ui/routes/ProtectedRoute.tsx
@@ -33,33 +33,3 @@ const ProtectedRoute = () => {
 }
 
 export default ProtectedRoute
-
-
-/*
-const ProtectedRoute = () => {
-  const navigate = useNavigate();
-  const setUsername = useSetUsername();
-  const [retrieved, setRetrieved] = useState(false);
-
-  const checkToken = async () => {
-    try {
-      const response = await meService.getUsername();
-      setUsername(response.data.username);
-      setRetrieved(true);
-    } catch (err) {
-      setRetrieved(false);
-      console.log(err);
-      navigate("/login");
-    }
-  }
-  useEffect(() => {
-    checkToken();
-  }, [])
-
-  if (retrieved) return <Outlet />
-  return null;
-}
-
-export default ProtectedRoute
-
-*/
